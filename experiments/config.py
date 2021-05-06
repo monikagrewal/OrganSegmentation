bdd69f43797d3afea9626a66a38880aed1e3ac1b
hgk <geert.klop@cwi.nl>
2021-05-06 10:58:45 +0200

    Implement defaults

--- experiments/config.py
+++ experiments/config.py
@@ -12,7 +12,7 @@ class Config(BaseSettings):
 
     # Data
     CLASSES: list[str] = ["background", "spleen"]
-    DATA_DIR: str = "../data/raw/Task09_Spleen"
+    DATA_DIR: str = "/export/scratch3/bvdp/data/Task09_Spleen"
     META_PATH: str = "dataset.json"
     # ROOT_DIR = "/export/scratch2/bvdp/Data/Projects_DICOM_data/ThreeD/MODIR_data_train_split_preprocessed_21-08-2020/"  # noqa
     # META_PATH = "/export/scratch3/bvdp/segmentation/OAR_segmentation/data_preparation/meta/dataset_train_21-08-2020_slice_annot.csv"  # noqa
@@ -23,32 +23,34 @@ class Config(BaseSettings):
     MODEL_WIDTH: int = 64  # network width
     IMAGE_DEPTH: int = 32
 
+    # Preprocessing
+    GAMMA: bool = 1
+    ALPHA: bool = None
+    IMAGE_SCALE_INPLANE: Optional[int] = None
+    AUGMENTATION_BRIGHTNESS: dict = dict(p=0.5, rel_addition_range=(-0.2, 0.2))
+    AUGMENTATION_CONTRAST: dict = dict(p=0.5, contrast_mult_range=(0.8, 1.2))
+    AUGMENTATION_ROTATE3D: dict = dict(
+        p=0.3, x_range=(-10, 10), y_range=(-10, 10), z_range=(-10, 10)
+    )
+
     # Training
-    NEPOCHS: int = 100
-    EARLY_STOPPING_PATIENCE: Optional[int] = None  # (None = deactivate)
-    LR: float = 1e-3  # learning rate
-    WEIGHT_DECAY: float = 1e-4
+    NEPOCHS: int = 1
     BATCHSIZE: int = 1
     ACCUMULATE_BATCHES: int = 1
+    LR: float = 1e-3  # learning rate
+    WEIGHT_DECAY: float = 1e-4
     LOSS_FUNCTION: str = "soft_dice"
     CLASS_WEIGHTS: Optional[list[int]] = None
     CLASS_SAMPLE_FREQS: list[int] = [1, 1, 1, 1, 1]  # sample freq weight per class
+    EARLY_STOPPING_PATIENCE: Optional[int] = None  # (None = deactivate)
 
     # Validation
-    PROPER_EVAL_EVERY_EPOCHS: int = 1
+    PROPER_EVAL_EVERY_EPOCHS: int = None
 
     # for sliding window validation, overlapping slice windows passed to the model.
     # If true, apply gaussian weighting so that the predictions in center of the window
     # have more weight on the final prediction for a voxel
     SLICE_WEIGHTING: bool = True
-
-    # Preprocessing
-    GAMMA: bool = 1
-    ALPHA: bool = None
-    IMAGE_SCALE_INPLANE: Optional[int] = None
-    AUGMENTATION_BRIGHTNESS: int = 0
-    AUGMENTATION_CONTRAST: int = 0
-    AUGMENTATION_ROTATE3D: int = 0
 
     # Logging Directories
     OUT_DIR: str = "./runs/tmp"
