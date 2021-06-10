def53776347c64f3dc2526db3cd7871820b165e3
hgk <geert.klop@cwi.nl>
2021-06-10 13:04:08 +0200

    Add support for .env file and add experiment name to config for logging

--- experiments/config.py
+++ experiments/config.py
@@ -7,19 +7,20 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     # General
+    EXPERIMENT_NAME: str = "all_classes"
     MODE: str = "train"
-    DEVICE: str = "cuda:0" if torch.cuda.is_available() else "cpu"
+    DEVICE: str = "cuda:1" if torch.cuda.is_available() else "cpu"
+    CLASSES: list[str] = ["background", "bowel_bag", "bladder", "hip", "rectum"]
 
     # Data
-    CLASSES: list[str] = ["background", "bowel_bag", "bladder", "hip", "rectum"]
     DATA_DIR: str = "/export/scratch2/grewal/Data/Projects_DICOM_data/ThreeD/MODIR_data_train_split_preprocessed_21-08-2020"  # noqa
     META_PATH: str = "../data_preparation/meta/dataset_train_21-08-2020_slice_annot.csv"
 
     # Unet
     LOAD_WEIGHTS: bool = False
     MODEL_DEPTH: int = 4  # network depth
-    MODEL_WIDTH: int = 16  # network width
-    IMAGE_DEPTH: int = 16
+    MODEL_WIDTH: int = 64  # network width
+    IMAGE_DEPTH: int = 32
 
     # Preprocessing
     GAMMA: int = 1
@@ -51,7 +52,7 @@ class Config(BaseSettings):
     TEST_ON_TRAIN_DATA: bool = False
 
     # Logging Directories
-    OUT_DIR: str = "../runs/tmp"
+    OUT_DIR: str = f"../runs/{EXPERIMENT_NAME}"
     OUT_DIR_TRAIN: str = os.path.join(OUT_DIR, "train")
     OUT_DIR_VAL: str = os.path.join(OUT_DIR, "val")
     OUT_DIR_PROPER_VAL: str = os.path.join(OUT_DIR, "proper_val")
@@ -63,6 +64,10 @@ class Config(BaseSettings):
         ("training" if TEST_ON_TRAIN_DATA else "test")
         + ("_postprocess" if POSTPROCESSING else ""),
     )
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 config = Config()
