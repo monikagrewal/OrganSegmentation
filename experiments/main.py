0465e6b992cdb7aff3578c415ad4deac56c49413
Dustin van Weersel <dagvw@cwi.nl>
2022-10-10 12:46:46 +0200

    Added try except around main

--- experiments/main.py
+++ experiments/main.py
@@ -22,17 +22,21 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-    start_time = datetime.now()
-    logging.info(f"Start time: {start_time}")
-    if cli_args.env_file:
-        # Train model and test on validation dataset
-        logging.info("Training model")
-        setup_train()
-    elif cli_args.out_dir:
-        # Run model on test dataset
-        logging.info("Testing model")
-        setup_test(out_dir=cli_args.out_dir)
-    else:
-        logging.warning("No env file supplied or out dir specified.")
-    end_time = datetime.now()
-    logging.info(f"End time: {end_time}. Duration: {end_time - start_time}")
+    try:
+        start_time = datetime.now()
+        logging.info(f"Start time: {start_time}")
+        if cli_args.env_file:
+            # Train model and test on validation dataset
+            logging.info("Training model")
+            setup_train()
+        elif cli_args.out_dir:
+            # Run model on test dataset
+            logging.info("Testing model")
+            setup_test(out_dir=cli_args.out_dir)
+        else:
+            logging.warning("No env file supplied or out dir specified.")
+        end_time = datetime.now()
+        logging.info(f"End time: {end_time}. Duration: {end_time - start_time}")
+    except Exception as e:
+        logging.warning(e)
+        raise e
